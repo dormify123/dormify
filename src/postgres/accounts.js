88978438020823f92f46cce5321dcf65d2e6a9b9
ezzedine <ezzedineozone123@gmail.com>
2024-02-22 15:50:11 +0200

    esbalished successful and anonymous connection to database

--- src/postgres/accounts.js
+++ src/postgres/accounts.js
@@ -1,3 +1,12 @@
-const {createClient} = supabase;
-const _supabase = supabase.createClient("https://ieioqboeihxduwuipmim.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaW9xYm9laWh4ZHV3dWlwbWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0MzA1MTAsImV4cCI6MjAyNDAwNjUxMH0.VLnvpD2Ejft5ZeUtZODHZJkcsI7JbwNK_zv24iazbbk");
-console.log(_supabase);
+function loadWrapperFunctions(supabase)
+{
+    console.log(supabase);
+    const _supabase = supabase.createClient("https://ieioqboeihxduwuipmim.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaW9xYm9laWh4ZHV3dWlwbWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0MzA1MTAsImV4cCI6MjAyNDAwNjUxMH0.VLnvpD2Ejft5ZeUtZODHZJkcsI7JbwNK_zv24iazbbk");
+    console.log(_supabase);
+    async function getUsers()
+    {
+        const {data,error} = await _supabase.from('users').select();
+        console.log(data);
+    }
+    getUsers();
+}
