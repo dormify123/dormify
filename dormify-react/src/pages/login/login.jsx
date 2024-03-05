656464a0d60a5a8bf4500abc576bfdc98f637607
Steven Oueiss <svo00@mail.aub.edu>
2024-03-05 14:24:39 +0200

    moved login to react

---
+++ dormify-react/src/pages/login/login.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import './login.css';
+
+const Login = () => {
+
+  return (
+    <>
+      <form id="loginForm" autoComplete="on">
+        <div className="container">
+          <h1>Login</h1>
+          <label htmlFor="fname">First name</label>
+          <input
+            type="text"
+            placeholder="Type your first name..."
+            id="fname"
+            name="fname"
+            required
+          />
+          <label htmlFor="lname">Last name</label>
+          <input
+            type="text"
+            placeholder="Type your last name..."
+            id="lname"
+            name="lname"
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="user_password"
+            type="password"
+            placeholder="Type your password..."
+            name="password"
+            required
+          />
+          <input type="checkbox" name="remember" /> Remember password
+          <button type="submit" className="loginbtn">
+            Login
+          </button>
+          <p>
+            Don't have an account?{" "}
+            <a>Sign up here</a>.
+          </p>
+          <div id="loginError" className="error-message"></div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Login;
