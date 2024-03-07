d9eaa60399d1943165247fcfe5a5ad0173d565ec
ezzedineozone <ezzedineozone123@gmail.com>
2024-03-07 14:43:47 +0200

    added placeholder homepage to start working on page routing in js

--- dormify-react/src/App.jsx
+++ dormify-react/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Login from './pages/login/login';
 import Header from './modules/header/header'
+import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        <Login/>
+        <Home/>
     </div>
   );
 }
---
+++ dormify-react/src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import './home.css';
+const Home = () => {
+    return(
+        <>
+            <div className="placeholder">
+                <h class ="placeholder-text">Home page</h>
+            </div>
+        </>
+    );
+};
+
+export default Home;
