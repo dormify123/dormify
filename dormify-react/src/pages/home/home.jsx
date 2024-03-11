e0fc9d583e543f8da4eb71cf02c770a90a8bc96e
Steven Oueiss <svo00@mail.aub.edu>
2024-03-11 16:42:40 +0200

    fixed some homepage stuff

--- dormify-react/src/pages/home/home.jsx
+++ dormify-react/src/pages/home/home.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./home.css";
-import dormImg from '../../assets/Transparent-pic.png';
+import dormImg from "../../assets/Transparent-pic.png";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleRegisterClick = () => {
+    navigate("/signup");
+  };
+
   return (
     <>
       <div className="home-container">
         <div className="hero-content">
           <h1 className="shadowed">Streamlining</h1>
-          <h1 style={{ marginTop: "-10px" }}>
-            <span className="blue-dark shadowed">Dorm Life</span>
+          <h1 className="title-negative-margin">
+            <span className="shadowed blue-dark">Dorm Life</span>
           </h1>
           <p className="shadowed grey-text">
             Implementing Solutions for Efficient Dormitory Management
           </p>
-          <a href="#" className="btn shadowed">Register</a>
+          <button className="btn-register" onClick={handleRegisterClick}>
+            Register
+          </button>
         </div>
         <div className="hero-image">
           <img className="img1" src={dormImg} alt="Dorm" />
