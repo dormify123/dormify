32ecf64c4e10f052c1ade77801a244249fa77680
ezzedineozone <ezzedineozone123@gmail.com>
2024-03-25 13:47:45 +0200

    Begin work on service page, backend mostly done, frontend needs more work

--- dormify-react/src/pages/services/services.jsx
+++ dormify-react/src/pages/services/services.jsx
@@ -1,14 +1,33 @@
-import BtnMedium from '../../modules/buttons/medium/btn-medium';
+import './services.css'
+import BtnMedium from '../../modules/buttons/medium/btn-medium'
 import {registerUserRole} from '../../utils/services/users'
 const Services = (session_) => {
     let {session} = session_;
     let {user} = session;
-    function onRegisterResidentClick() {
-        registerUserRole(user, "resident");
+    async function onRegisterRoleClick(role) {
+        console.log(role);
+        let message = await registerUserRole(user, role);
+        console.log(message);
     }
     return (
         <>
-           <BtnMedium onClick={onRegisterResidentClick}>Register as resident</BtnMedium> 
+        <div className = "wrapper">
+            <div className = "message-box">
+                <div className = "services-message">
+                    <h className = "message-text-large">Looks like you dont belong to any dorm</h>
+                    <h className = "message-text-small">Let's get you started</h>
+                </div>
+                <div className="services-box">
+                    <div className = "services-message box">
+                    <h className ="message-text-small">I am a:</h>
+                    </div>
+                    <BtnMedium className="services-row-box" withBackground={true} withBorder={true}>Resident</BtnMedium>
+                    <BtnMedium className="services-row-box" withBackground={true} withBorder={true}>Dorm Owner</BtnMedium>
+                </div>
+            </div>
+
+        </div>
+
         </>
     )
 }
--- dormify-react/src/utils/services/users.jsx
+++ dormify-react/src/utils/services/users.jsx
@@ -11,6 +11,10 @@ async function createUser(user_session, first_name, last_name, room_number){
         console.log("something went wrong inserting a user: " + error.message);
     return error;
 };
+async function createDorm(user_session, rooms, location, email)
+{
+    
+}
 async function getUserInformation(user_session)
 {
     let {id} = user_session;
@@ -20,20 +24,57 @@ async function getUserInformation(user_session)
 async function registerUserRole(user_session, role)
 {
     let {id} = user_session;
-    let error_resident;
+    let existing_role = await getUserRole(user_session);
+    let error_table;
     let error_roles;
     let sequence_error;
-    if(role === "resident"){
-        let {error,data} = await supabase.from('sequence').select('value').eq('table_name','resident');
-        let res_id = data[0].value;
-        error_resident = await supabase.from('resident').insert({id: res_id,dorm_id:null, room_num:null,user_id:id});
-        error_roles = await supabase.from('roles').insert({user_id:id, resident_id:res_id});
-        res_id +=1
-        sequence_error = await supabase.from('sequence').update({value: res_id}).eq('table_name', 'resident');
-        
+    console.log(existing_role);
+    if(!existing_role){
+        if(role === "resident"){
+            let {error,data} = await supabase.from('sequence').select('value').eq('table_name','resident');
+            let res_id = data[0].value;
+            error_table = await supabase.from('resident').insert({id: res_id,dorm_id:null, room_num:null,user_id:id});
+            error_roles = await supabase.from('roles').insert({user_id:id, resident_id:res_id});
+            res_id +=1
+            sequence_error = await supabase.from('sequence').update({value: res_id}).eq('table_name', 'resident');
+        }
+        else if(role === "staff")
+        {
+            let {error,data} = await supabase.from('sequence').select('value').eq('table_name','staff');
+            let stff_id = data[0].value;
+            error_table = await supabase.from('staff').insert({id: stff_id,user_id:id});
+            error_roles = await supabase.from('roles').insert({user_id:id, staff_id:stff_id});
+            stff_id +=1
+            sequence_error = await supabase.from('sequence').update({value: stff_id}).eq('table_name', 'staff');
+        }
+        else if(role === "dormowner")
+        {
+            let {error,data} = await supabase.from('sequence').select('value').eq('table_name','dormowner');
+            let drmowner_id = data[0].value;
+            error_table = await supabase.from('dormowner').insert({id: drmowner_id,user_id:id});
+            error_roles = await supabase.from('roles').insert({user_id:id, dormowner_id:drmowner_id});
+            drmowner_id +=1
+            sequence_error = await supabase.from('sequence').update({value: drmowner_id}).eq('table_name', 'dormowner'); 
+        }
     }
-    return {error_resident, error_roles, sequence_error};
+    else{
+        return {message:"User already has a role"};
+    }
+
+    return {error_table, error_roles, sequence_error};
+}
+async function getUserRole(user_session)
+{
+    let {id} = user_session;
+    let {error,data} = await supabase.from('roles').select('*').eq('user_id', id);
+    if(data.length === 0)
+        return null;
+    if(data[0].dormowner_id)
+        return "dormowner";
+    else if(data[0].resident_id)
+        return "resident";
+    else if(data[0].staff_id)
+        return "staff";
 }
 
-
-export{createUser, getUserInformation, registerUserRole};
+export{createUser, getUserInformation, registerUserRole, getUserRole};
