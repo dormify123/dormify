958d89bfbe214a11d870a6816cbd90decd06b0c6
ezzedineozone <ezzedineozone123@gmail.com>
2024-04-18 17:40:21 +0300

    added some forgotten files

---
+++ dormify-react/src/modules/tables/residents/residentsTable.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import './residentsTable.css';
+
+const defaultData = [
+    {
+        users:{
+            id: "1234",
+            full_name:"placeholder1"
+        }
+    },
+    {
+        users:{
+            id: "4321",
+            full_name:"placeholder2"
+        }
+    }
+];
+
+const ResidentsTable = ({title, custom_data, handleButtonClick}) => {
+  const [residentsTableData, setresidentsTableData] = useState(defaultData);
+  useEffect(() => {
+    if (custom_data) {
+      console.log("custom data updated");
+      console.log(custom_data);
+      setresidentsTableData(custom_data);
+    }
+  }, [custom_data]);
+  return (
+    <div className="scroll-container">
+      <div className="table-title">
+        {title}
+      </div>
+      <div className="content">
+        {residentsTableData.map((resident, index) => (
+          <div key={index} className="entry" onClick={() => {handleButtonClick(resident.users.id)}} id = {resident.users.id} ><div className="entry-text">{resident.users.full_name}</div></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ResidentsTable;
